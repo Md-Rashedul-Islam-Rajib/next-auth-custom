6b986d7fda7ec095bcb647cfb27fd2027da52975
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-17 03:20:58 +0600

    signup api created

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async(request : NextRequest) => {
+    try {
+        const userInfo = await request.json();
+        console.log(userInfo);
+        return NextResponse.json(userInfo);
+    } catch (error:any) {
+        return NextResponse.json({message: error.message, status: 500})
+    }
+}
