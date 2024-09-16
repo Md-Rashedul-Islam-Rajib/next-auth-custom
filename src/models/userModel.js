f7ba0f2931a1d1f53c18cdade1e347dab08531d7
Md Rashedul Islam <rajib5570@gmail.com>
2024-09-17 03:56:22 +0600

    model and  userschema added

---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true,"Name is required"]
+    },
+    email: {
+        type: String,
+        require: [true, "Email is require"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: [true, "Password is required"]
+    },
+    date:{
+    type: Date,
+    default: Date.now,
+    },
+    isVerified: {
+        type: boolean,
+        default: false,
+    },
+    verifyToken: String,
+    verifyTokenExpired: Date,
+})
+
